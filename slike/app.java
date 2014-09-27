01968e32ba7ced791f5de58be6f97703cbdba2a9
Saxhax <akedxs@gmail.com>
2014-09-28 00:56:51 +0200

    Slike app install

---
+++ slike/app.java
@@ -0,0 +1,19 @@
+import slike.java
+
+public class app{
+	public app(){}
+	public void run(){
+		//more commands 
+	}
+	public first(){
+		//this
+		//is
+		//new
+		//method
+	}
+	public second(){
+		//more commands in this 
+		//method
+	}
+}
+  
